f3df226022c01f245b22085bb534f5b37f202800
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-04-18 02:03:52 +0530

    initial

--- resources/views/includes/admin/footer.blade.php
+++ resources/views/includes/admin/footer.blade.php
@@ -75,7 +75,7 @@
     <!-- <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script> -->
     <!-- <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script> -->
     <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <!-- <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script> -->
     <script src="{{ asset('js/custom-admin.js') }}"></script>
 
     
