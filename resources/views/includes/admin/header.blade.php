9c1071eb808078e10057bbb593adfe804e7dff12
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-04-18 15:30:41 +0530

    initial

--- resources/views/includes/admin/header.blade.php
+++ resources/views/includes/admin/header.blade.php
@@ -19,6 +19,8 @@
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <!-- fontawsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Custom css -->
     <link rel="stylesheet" href="{{ asset('css/base-style.css')}}">
