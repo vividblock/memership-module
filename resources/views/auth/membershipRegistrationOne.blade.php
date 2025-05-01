bcabf74191652c63c7bc016a9bf667ed1cd29a65
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-05-01 16:15:32 +0530

    initial

--- resources/views/auth/membershipRegistrationOne.blade.php
+++ resources/views/auth/membershipRegistrationOne.blade.php
@@ -178,9 +178,10 @@
             const OtpStatus = $("input[name='email_otp_status']").val();
             function checkOtpValidateOrNot(){
                 if(OtpStatus === "true"){
-                    console.log("otp Validated");
+                    // console.log("otp Validated");
                     $("#submit-btn").prop("disabled", false);
-                    $("#email-verify-auth-page").show().html('<i class="fa-solid fa-check-double"></i>');
+                    $("#email-verify-auth-page").show();
+                    $("#email-verify-auth-page").html('<i class="fa-solid fa-check-double"></i>');
                 }
             }
 
