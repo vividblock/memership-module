85353300c9f2cb8be7dc9d737b6a8f6e50e70b24
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-05-08 02:08:44 +0530

    initial

--- resources/views/includes/members/footer.blade.php
+++ resources/views/includes/members/footer.blade.php
@@ -53,11 +53,11 @@
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
     <!-- Page level plugins -->
-    <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+    <!-- <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script> -->
 
     <!-- Page level custom scripts -->
-    <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
-    <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
+    <!-- <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script> -->
+    <!-- <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script> -->
 
 
 
