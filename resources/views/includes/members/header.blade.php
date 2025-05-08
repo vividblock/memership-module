5187222add21ef9898d1fad0544bc96dee22b66e
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-05-08 21:18:39 +0530

    initial

--- resources/views/includes/members/header.blade.php
+++ resources/views/includes/members/header.blade.php
@@ -62,22 +62,51 @@
                     <i class="fa-regular fa-id-card"></i>
                     <span>Profile</span>
                 </a>
+
+                <a class="nav-link" href="#">
+                    <i class="fa-regular fa-id-card"></i>
+                    <span>Profile</span>
+                </a>
             </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading
+            <!-- Heading  -->
             <div class="sidebar-heading">
-                Leads
-            </div> -->
+                Listing
+            </div> 
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Settings</span>
+                    <span>Listing</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <!-- <h6 class="collapse-header">Notification & </h6> -->
+                        <a class="collapse-item" href="#">Add Listing <span class="working-on-dashboard-option"><i class="fa-solid fa-hammer"></i></span></a>
+                        <a class="collapse-item" href="#">View Listing</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading  -->
+            <div class="sidebar-heading">
+                Support & Notification
+            </div> 
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Support & Notification</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
