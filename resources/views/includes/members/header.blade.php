bb3016844db239dbe86675bf3ee6be34943496a3
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-05-09 03:02:31 +0530

    initial

--- resources/views/includes/members/header.blade.php
+++ resources/views/includes/members/header.blade.php
@@ -123,6 +123,29 @@
                 </div>
             </li>
 
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading  -->
+            <div class="sidebar-heading">
+                Settings
+            </div> 
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
+                    aria-expanded="true" aria-controls="collapseOne">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Settings</span>
+                </a>
+                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <!-- <h6 class="collapse-header">Notification & </h6> -->
+                        <a class="collapse-item" href="#">Reset Password</a>
+                    </div>
+                </div>
+            </li>
+
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
