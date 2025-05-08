f5e50e3fd97f543b565ffe4079178cadc21d7450
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-05-08 21:27:09 +0530

    initial

--- resources/views/includes/members/header.blade.php
+++ resources/views/includes/members/header.blade.php
@@ -47,7 +47,7 @@
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
+            <li class="nav-item {{ request()->routeIs('membersDashboard') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('membersDashboard') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span>
