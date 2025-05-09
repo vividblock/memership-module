e7fb4157d3404fdbbd5faa8bb4b23b902fab4f29
Tapabrata-vividblock <tapabrata@vividblock.com>
2025-05-09 15:56:12 +0530

    initial

--- resources/views/adminDashboard/waitingMembersSingleView.blade.php
+++ resources/views/adminDashboard/waitingMembersSingleView.blade.php
@@ -7,7 +7,7 @@
 
     <div class="row g-4">
         {{-- Member Details --}}
-        <div class="col-md-6">
+        <div class="col-md-6 mb-2">
             <div class="card shadow rounded-4 border border-primary-subtle">
                 <div class="card-header bg-primary text-white rounded-top-4 border-bottom">
                     <h5 class="mb-0">Member Details</h5>
@@ -32,7 +32,7 @@
         </div>
 
         {{-- Organisation Details --}}
-        <div class="col-md-6">
+        <div class="col-md-6 mb-2">
             <div class="card shadow rounded-4 border border-success-subtle">
                 <div class="card-header bg-success text-white rounded-top-4 border-bottom">
                     <h5 class="mb-0">Organisation Details</h5>
@@ -57,7 +57,7 @@
         </div>
 
         {{-- Organisation Description --}}
-        <div class="col-md-12">
+        <div class="col-md-12 mb-2">
             <div class="card shadow rounded-4 border border-info-subtle">
                 <div class="card-header bg-info text-white rounded-top-4 border-bottom">
                     <h5 class="mb-0">Organisation Description</h5>
@@ -76,7 +76,7 @@
 
         {{-- Extended Organisation Details --}}
         @if ($organisation_details)
-        <div class="col-md-6">
+        <div class="col-md-6 mb-2">
             <div class="card shadow rounded-4 border border-warning-subtle">
                 <div class="card-header bg-warning text-dark rounded-top-4 border-bottom">
                     <h5 class="mb-0">Additional Organisation Info</h5>
@@ -104,7 +104,7 @@
 
         {{-- Local Activities --}}
         @if ($organisation_local_activities)
-        <div class="col-md-6">
+        <div class="col-md-6 mb-2">
             <div class="card shadow rounded-4 border border-secondary-subtle">
                 <div class="card-header bg-secondary text-white rounded-top-4 border-bottom">
                     <h5 class="mb-0">Local Activities</h5>
@@ -132,7 +132,7 @@
 
         {{-- Network Survey --}}
         @if ($member_network_survey)
-        <div class="col-md-12">
+        <div class="col-md-12 mb-2">
             <div class="card shadow rounded-4 border border-dark-subtle">
                 <div class="card-header bg-dark text-white rounded-top-4 border-bottom">
                     <h5 class="mb-0">Network Survey</h5>
